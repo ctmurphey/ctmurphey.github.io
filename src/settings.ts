979fbb7b9fb21eec24d37fffe9811f3250244600
Chauncey Murphey <94994713+ctmurphey@users.noreply.github.com>
2025-04-09 05:27:45 -0500

    added more research areas

--- src/settings.ts
+++ src/settings.ts
@@ -5,7 +5,9 @@ export const profile = {
 	author_name: 'C. T. Murphey', // Author name to be highlighted in the papers section
 	research_areas: [
 		// { title: 'Physics', description: 'Brief description of the research interest', field: 'physics' },
-		{title: 'Supernovae', description: 'I search for very supernovae with the Dark Energy Camera.'}
+		{title: 'Supernovae', description: 'I search for very supernovae with the Dark Energy Camera.'},
+		{title: 'Data Science', description: "I'm an LSST DA Data Science Fellow analyzing hundreds of supernovae."},
+		{title: 'High Performance Computing', description: "I image process over 100TB of raw images on the Illinois Campus Cluster"}
 	],
 }
 
