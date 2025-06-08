f3a04b2725fb9ba4f68a0d9644e3dfcf00fe22eb
Chauncey Murphey <94994713+ctmurphey@users.noreply.github.com>
2025-06-08 10:01:00 -0500

    filled out rest of cv

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -62,16 +62,36 @@ export const education = [
 
 export const skills = [
 	{
-		title: '',
-		description: '',
+		title: 'Programming Languages',
+		description: 'Python, Bash, C/C++, Julia, Perl, SQL',
 	},
-	// {
-	// 	title: 'Experimental Techniques',
-	// 	description: 'Spectroscopy, Isolation of Radioactive Elements, Radiation Measurement',
-	// },
+	{
+		title: 'Libraries',
+		description: 'NumPy, SciPy, AstroPy, Matplotlib, Pandas, PyTorch, Scikit-learn, Tensorflow, PyMC',
+	},
+	{
+		title: 'Frameworks and Tools',
+		description: 'Jupyter, SSH, VS Code, Vim, Obsidian',
+	},
 ];
 
 export const publications = [
+	{
+		title: 'JWST and Ground-based Observations of the Type Iax Supernovae SN 2024pxl and SN 2024vjm: Evidence for Weak Deflagration Explosions',
+		authors: 'Lindsey A. Kwok, Mridweeka Singh, et al. (incl. C. Tanner Murphey)',
+		journal: 'Submitted to AAS Journals',
+		time: '2025',
+		link: 'https://arxiv.org/abs/2505.02944',
+		abstract: 'We present panchromatic optical + near-infrared (NIR) + mid-infrared (MIR) observations of the intermediate-luminosity Type Iax supernova (SN Iax) 2024pxl and the extremely low-luminosity SN Iax 2024vjm. JWST observations provide unprecedented MIR spectroscopy of SN Iax, spanning from +11 to +42 days past maximum light. We detect forbidden emission lines in the MIR at these early times while the optical and NIR are dominated by permitted lines with an absorption component. Panchromatic spectra at early times can thus simultaneously show nebular and photospheric lines, probing both inner and outer layers of the ejecta. We identify spectral lines not seen before in SN Iax, including [Mg II] 4.76 µm, [Mg II] 9.71  µm, [Ne II] 12.81 µm, and isolated O I 2.76 µm that traces unburned material. Forbidden emission lines of all species are centrally peaked with similar kinematic distributions, indicating that the ejecta are well mixed in both SN 2024pxl and SN 2024vjm, a hallmark of pure deflagration explosion models. Radiative transfer modeling of SN 2024pxl shows good agreement with a weak deflagration of a near-Chandrasekhar-mass white dwarf, but additional IR flux is needed to match the observations, potentially attributable to a surviving remnant. We favor a weak deflagration origin for SN 2024vjm because of its panchromatic spectral similarity to SN 2024pxl, despite the large difference in luminosity. However, our comparison weak deflagration models are all too luminous and rapidly fading compared to SN 2024vjm; future modeling should push to even weaker explosions and include the contribution of a bound remnant. Our observations demonstrate the diagnostic power of panchromatic spectroscopy for unveiling explosion physics in thermonuclear supernovae. ,
+	},
+	{
+		title: 'Photometry and Spectroscopy of SN 2024pxl: A Luminosity Link Among Type Iax Supernovae',
+		authors: 'Mridweeka Singh, Lindsey A. Kwok, et al. (incl. C. Tanner Murphey)',
+		journal: 'Submitted to ApJ',
+		time: '2025',
+		link: 'https://arxiv.org/abs/2505.02943',
+		abstract: 'We present extensive ultraviolet to optical photometric and optical to near-infrared (NIR) spectroscopic follow-up observations of the nearby intermediate-luminosity (-16.81+/-0.19~mag) Type Iax supernova (SN) 2024pxl in NGC 6384. SN~2024pxl exhibits a faster light curve evolution than the high-luminosity members of this class, and slower than low-luminosity events. The observationally well-constrained rise time of ~10 days and an estimated synthesized 56Ni mass of 0.03 Msun, based on analytical modeling of the pseudobolometric light curve, are consistent with models of the weak deflagration of a carbon-oxygen white dwarf. Our optical spectral sequence of SN~2024pxl shows weak \ion{Si}{2} lines and spectral evolution similar to other high-luminosity Type Iax SNe, but also prominent early-time \ion{C}{2} line, like lower-luminosity Type Iax SNe. The late-time optical spectrum of SN~2024pxl closely matches that of SN 2014dt, and its NIR spectral evolution aligns with those of other well-studied, high-luminosity Type Iax SNe. The spectral-line expansion velocities of SN~2024pxl are at the lower end of the Type Iax SN velocity distribution, and the velocity distribution of iron-group elements compared to intermediate-mass elements suggests that the ejecta are mixed on large scales, as expected in pure deflagration models. SN~2024pxl exhibits characteristics intermediate between those of high-luminosity and low-luminosity Type~Iax SNe, further establishing a link across this diverse class. ',
+	},
 	{
 		title: 'Witnessing history: sky distribution, detectability, and rates of naked-eye Milky Way supernovae',
 		authors: ' Murphey, C. Tanner, et al.',
@@ -80,12 +100,13 @@ export const publications = [
 		link: 'https://academic.oup.com/mnras/article/507/1/927/6330468',
 		abstract: 'The Milky Way hosts on average a few supernova explosions per century, yet in the past millennium only five supernovae have been identified confidently in the historical record. This deficit of naked-eye supernovae is at least partly due to dust extinction in the Galactic plane. We explore this effect quantitatively, developing a formalism for the supernova probability distribution in space and on the sky, accounting for dust and for the observer\'s flux limit. We then construct a fiducial axisymmetric model for the spatial supernova and dust densities, featuring an exponential dependence on galactocentric radius and height, with core-collapse events in a thin disc and Type Ia events including a thick disc component. When no flux limit is applied, our model predicts that on the sky, supernovae are intrinsically concentrated in the Galactic plane, with Type Ia events extending to higher latitudes. We then apply a flux limit and include dust effects, to predict the sky distribution of historical supernovae. We use well-observed supernovae as light-curve templates, and introduce naked-eye discovery criteria. The resulting sky distributions are strikingly inconsistent with the locations of confident historical supernovae, none of which lie near our model\'s central peaks. Indeed, SN 1054 lies off the plane almost exactly in the anticentre, and SN 1181 is in the second Galactic quadrant. We discuss possible explanations for these discrepancies. We calculate the percentage of all supernovae bright enough for historical discovery: ≃13 per cent of core-collapse and ≃33 per cent of Type Ia events. Using these and the confident historical supernovae, we estimate the intrinsic Galactic supernova rates, finding general agreement with other methods. Finally, we urge searches for supernovae in historical records from civilizations in the Southern hemisphere.',
 	},
-	// {
-	// 	title: 'The Radiation of Uranium Compounds',
-	// 	authors: 'Marie Curie',
-	// 	journal: 'Comptes Rendus de l’Académie des Sciences',
-	// 	time: '1898',
-	// 	link: '#',
-	// 	abstract: 'Early research leading to the identification of uranium’s radioactive properties.',
-	// },
+	{
+		title: 'The Plane\'s The Thing: The Case for Wide-Fast-Deep Coverage of the Galactic Plane and Bulge',
+		authors: 'Jay Strader, et al. (incl. C. Tanner Murphey)',
+		journal: 'arXiv',
+		time: '2018',
+		link: 'https://arxiv.org/abs/1811.12433',
+		abstract: 'We argue that the exclusion of the Galactic Plane and Bulge from the uniform wide-fast-deep (WFD) LSST survey cadence is fundamentally inconsistent with two of the main science drivers of LSST: Mapping the Milky Way and Exploring the Transient Optical Sky. We outline the philosophical basis for this claim and then describe a number of important science goals that can only be addressed by WFD-like coverage of the Plane and Bulge.',
+	},
+	
 ];
